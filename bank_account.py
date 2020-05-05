91eae226f3c29325e2e6ff454319ea07b1a2b7ab
Miguel Johnson <miguelbjohnson@gmail.com>
2020-05-05 19:08:39 -0400

    Set item prices for the store

--- bank_account.py
+++ bank_account.py
@@ -5,6 +5,25 @@ initial_deposit = int(input('How much money would you like to add: '))
 
 bank_account = BankAccount(name)
 bank_account.add_deposit(initial_deposit)
-#bank_account.make_withdrawal(11)
+
+
+exit_status = ''
+while exit_status.lower() != 'q':
+    print("""
+    Please choose an item:
+
+    1. Apples: $5
+    2. Oat Milk: $3
+    3. Crab Legs: $40
+    4.Deposit more funds
+    """ )
+
+    choice = int(input())
+    
+    if choice == 1:
+        bank_account.make_withdrawal(5)
+        print(f"Your current balance is{bank_account.balance}")
+        
+    exit_status = input("Hit any key to continue or 'q' to quit")
 
 print(bank_account.__dict__)
