086b0d7d586bcb24c985c6370e48aa588c36424c
Miguel Johnson <miguelbjohnson@gmail.com>
2020-05-05 18:18:51 -0400

    Added make_withdraw method

--- bank_account.py
+++ bank_account.py
@@ -6,7 +6,11 @@ class BankAccount(object):
     def add_deposit(self, amount):
         self.balance += amount
 
+    def make_withdraw(self, amount):
+        self.balance -= amount
+
 bank_account = BankAccount('Miguel')
 bank_account.add_deposit(10)
+bank_account.make_withdraw(5)
 print(bank_account.__dict__)  
 
