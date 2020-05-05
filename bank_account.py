042acfb7f5e4fa74757edbd8e5aded689a745a88
Miguel Johnson <miguelbjohnson@gmail.com>
2020-05-05 18:16:36 -0400

    Set up add_deposit method

--- bank_account.py
+++ bank_account.py
@@ -3,7 +3,10 @@ class BankAccount(object):
         self.name = object
         self.balance = 0
 
+    def add_deposit(self, amount):
+        self.balance += amount
 
 bank_account = BankAccount('Miguel')
+bank_account.add_deposit(10)
 print(bank_account.__dict__)  
 
